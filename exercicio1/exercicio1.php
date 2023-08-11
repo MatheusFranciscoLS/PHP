ad54862bd9332fe0c09045ae67710ccf01e08393
MatheusFranciscoLS <matheusfran.ls@gmail.com>
2023-08-10 21:48:03 -0300

    first commit

--- exercicio1/exercicio1.php
+++ exercicio1/exercicio1.php
@@ -13,6 +13,7 @@
 
 	$str3 = "O PHP foi criado em mil novecentos e noventa e cinco ";
 	$str3 = str_replace("o", "0", "$str3");
+	$str3 = str_replace("O", "0", "$str3");
 	$str3 = str_replace("a", "4", "$str3");
 	$str3 = str_replace("i", "1", "$str3");
 
