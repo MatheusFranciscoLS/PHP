147cbf86262371b9977ea29f16cd2e6d79ab840d
MatheusFranciscoLS <matheusfran.ls@gmail.com>
2023-08-24 21:56:30 -0300

    24/08/23

--- ExercicioFixacao/exercicio2/form.php
+++ ExercicioFixacao/exercicio2/form.php
@@ -3,7 +3,25 @@
 $farenheit = $_POST['farenheit'];
 $celsius = $_POST['celsius'];
 
-    echo "Celsius: " . ($farenheit - 32) * (5 / 9) . "<br>";
-    echo "Farenheit: " . $celsius * (9 / 5) + 32;
+function celsius(){
+    $farenheit = $_POST['farenheit'];
+    if ($farenheit != 0) {
+        echo "Temperatura em Celsius: " . ($farenheit - 32) * (5 / 9) . "<br>";
+    } else {
+        echo "Temperatura em Celsius está em -17,7778 <br>";
+    }
+}
+
+function farenheit(){
+    $celsius = $_POST['celsius'];
+    if ($celsius != 0) {
+        echo "Temperatura em Farenheit: " . $celsius * (9 / 5) + 32 . "<br>";
+    } else {
+        echo "Temperatura em Farenheit está em 32 <br>";
+    }
+}
+
+    echo celsius();
+    echo farenheit();
 
 ?>
