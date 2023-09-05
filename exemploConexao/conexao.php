5722cfcc9793aaa02459e9e0931220bf103a8957
MatheusFranciscoLS <matheusfran.ls@gmail.com>
2023-09-04 21:54:41 -0300

    04/09/23

---
+++ exemploConexao/conexao.php
@@ -0,0 +1,9 @@
+<?php
+//Dados de conexão com o banco de dados
+$servername = "127.0.0.1";
+$username = "root";
+$password = "";
+$dbname = "banco";
+
+//Tentativa de conexão
+$conn = mysqli_connect($servername, $username, $password, $dbname);
