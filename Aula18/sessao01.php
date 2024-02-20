c2d6ca8aef3198e7b2db40c8fb5e2eb6fce957e7
MatheusFranciscoLS <matheusfran.ls@gmail.com>
2024-02-19 21:54:11 -0300

    19/02/24

--- Aula18/sessao01.php
+++
@@ -1,4 +0,0 @@
-<?php
-session_start(); // precisa ser chamada antes toda vez.
-echo "Seu nome é " . $_SESSION['nome']; /* recupera o valor que veio junto com a requisição */
-?>
--- ExercicioFixacao/exercicio1/form.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$raio = $_POST['num'];
-
-echo "Raio: $raio <br>";
-
-echo "Perímetro: " . 2 * M_PI * $raio . "<br>";
-
-echo "Área: " . M_PI * $raio * $raio . "<br>";  
